2d85e0e4528ee6180a0d141b42ef7bdbfc4cc7b8
Nivedita Tamrakar <Nivedita.Tamrakar@bajajfinserv.in>
2024-10-23 10:41:20 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/nivedita_tamrakar_personal_finance_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nivedita_tamrakar_personal_finance_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const 
+  ProviderScope(child: PersonalFinanceManager()));
+}
+
+class PersonalFinanceManager extends ConsumerStatefulWidget {
+  const PersonalFinanceManager({super.key});
+
+  @override
+  ConsumerState<PersonalFinanceManager> createState() => _PersonalFinanceManagerState();
+}
+
+class _PersonalFinanceManagerState extends ConsumerState<PersonalFinanceManager> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
